140811d1b9b1a9f7a85defe7ca2fb90d3fcc2ebb
gj-ca <56330692+gj-ca@users.noreply.github.com>
2019-10-30 11:19:58 +1000

    Create 01_ruby_intro.rb

---
+++ 01_ruby_intro.rb
@@ -0,0 +1,10 @@
+# Welcome to the first of the Git Ruby Challenges. Updated on the daily.
+
+# Smaller and less intense than the Canvas challenges, these are designed to introduce a ruby methods and get you familiar with searching for a solution
+
+# First Challenge is to take my name and convert every third character to its ASCII value
+
+# For example,
+# my_name = "Rumpelstiltskin Smith"
+
+# So my name Should end up being "Ru109pe108st105lt105kin32Sm105th"
