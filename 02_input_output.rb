7fab30c1f9b9ad9c5458820e98fac35cf7586890
gj-ca <56330692+gj-ca@users.noreply.github.com>
2021-09-07 11:54:41 +1000

    update challenge notes

--- 02_input_output.rb
+++ 02_input_output.rb
@@ -17,10 +17,45 @@
 # Beast mode challenge:
 # Ater the above, the program calculates how many years until
 # the user is 100 years old and tells the user. For example, for
-# our user input above (Tom and 40), after outputing 
+# our user input above (Tom and 40), after the program outputs 
 # "Wow! You are 40 years old. Congratulations!"
 # The program also outputs:
 # "Tom, in 60 years you will be 100 years old!"
+# Our program isn't very nice though, when a user's age is over 100 the program will
+# instead output "Tom, you are super old".
+
+# Example 1:
+
+# Program is run and outputs:
+# "What is your name?"
+
+# User inputs: "Tom"
+
+# Program outputs:
+# "Hi Tom! How old are you?"
+
+# User inputs: 40
+
+# Program outputs:
+# "Wow! You are 40 years old. Congratulations!"
+# "Tom, in 60 years you will be 100 years old!"
+
+# Example 2:
+
+# Program is run and outputs:
+# "What is your name?"
+
+# User inputs: "Tom"
+
+# Program outputs:
+# "Hi Tom! How old are you?"
+
+# User inputs: 40
+
+# Program outputs:
+# "Wow! You are 40 years old. Congratulations!"
+# "Tom, you are super old!"
+
 
 # Beast mode++ challenge:
 # Turn this program into a loop that will repeat these tasks until they type 'stop'
